137efc0642dfb6d41747595c2ee06e48c202b85d
aun-afzaal <codewithaun@gmail.com>
2026-04-25 08:41:22 +0500

    text

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,10 +1,20 @@
+"use client";
+
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 
 const Avatar = () => {
+  const searchParams = useSearchParams();
+  const showAun = searchParams.get("aun") === "true";
+
+  const src = showAun
+    ? "https://www.abcodify.com/aun.png"
+    : "/avatar-new.png";
+
   return (
     <div className="hidden xl:flex xl:max-w-none pointer-events-none select-none">
       <Image
-        src="/avatar-new.png"
+        src={src}
         alt="avatar"
         width={737}
         height={678}
