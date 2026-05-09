920ada89295f4ca9396fac3146903cbaf5ca257c
aun-afzaal <codewithaun@gmail.com>
2026-05-09 22:11:19 +0500

    refactor: enhance testimonial section layout for improved readability
    - Wrap SectionLabel and heading in a div for better alignment
    - Adjust margin for heading to improve spacing

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -8,16 +8,18 @@ const Testimonials = () => {
   return (
     <div className="bg-[var(--navy-900)] xl:py-36 py-20 flex items-center w-full relative h-full">
       <div className="container mx-auto h-full flex flex-col md:justify-center">
-        <SectionLabel>Client feedback</SectionLabel>
-        <motion.h2
-          variants={fadeIn("up", 0.2)}
-          initial="hidden"
-          animate="show"
-          exit="hidden"
-          className="h2 mb-8 xl:mb-0 w-full text-center"
-        >
-          What clients <span className="text-accent">say.</span>
-        </motion.h2>
+        <div className="text-center">
+          <SectionLabel>Client feedback</SectionLabel>
+          <motion.h2
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2 mb-4 xl:mb-0 w-full"
+          >
+            What clients <span className="text-accent">say.</span>
+          </motion.h2>
+        </div>
 
         {/* slider */}
         <motion.div
