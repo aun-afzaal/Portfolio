8282d25132b4a7a725b1a4b7a0a94737265b467e
Muhammad  Aun <codewithaun@gmail.com>
2026-08-10 06:51:11 +0500

    Update next.config.js with export output and unoptimized images
    Added output and unoptimized options to Next.js config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
