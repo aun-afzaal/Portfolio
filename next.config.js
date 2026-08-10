645404b944098eaf15ae981e0afc5cc117581a52
Muhammad  Aun <codewithaun@gmail.com>
2026-08-10 07:16:03 +0500

    Refactor next.config.js by removing unused settings
    Removed output, basePath, assetPrefix, and unoptimized settings.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 const nextConfig = {
-  output: 'export',
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
